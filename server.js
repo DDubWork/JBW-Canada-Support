9b8ea5efc060f2ccc68b5c1b2262bd5126ce62a4
Damian <dwolecki@jbwarranties.com>
2025-10-02 14:01:30 -0400

    west

---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const { twiml } = require("twilio");
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Route for initial call
+app.post("/voice", (req, res) => {
+  const response = new twiml.VoiceResponse();
+
+  const gather = response.gather({
+    numDigits: 1,
+    timeout: 60,
+    action: "/handle-key", // Twilio will POST here
+  });
+
+  gather.say(
+    "Welcome to JB Warranties Canada support. Press 1 to connect to Alex Bulanov. Press 2 to connect to Sam Arbour.",
+    { bargeIn: true }
+  );
+
+  response.say("We did not receive any input. Goodbye!");
+
+  res.type("text/xml");
+  res.send(response.toString());
+});
+
+// Route for handling keypress
+app.post("/handle-key", (req, res) => {
+  const response = new twiml.VoiceResponse();
+  const digit = req.body.Digits;
+
+  if (digit === "1") {
+    response.say("Please hold for Alex Bulanov");
+    response.dial("+12892007026");
+  } else if (digit === "2") {
+    response.say("Please hold for Sam Arbour");
+    response.dial("+12894235553");
+  } else {
+    response.say("Invalid choice, goodbye.");
+  }
+
+  res.type("text/xml");
+  res.send(response.toString());
+});
+
+// Railway needs this
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));

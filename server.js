0d5e733c28cb30e78208711dbd83e8ba6ddc7e7e
Damian <dwolecki@jbwarranties.com>
2025-10-03 12:23:03 -0400

    Splitting up code

--- server.js
+++ server.js
@@ -1,46 +1,25 @@
 const handleKey = require("./handleKey.js");
-
-console.log("🚀 Starting server.js...");
-
+const voice = require("./voice.js");
 
 const express = require("express");
 const bodyParser = require("body-parser");
 const { twiml } = require("twilio");
-
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 
+console.log("🚀 Starting server.js...");
 
 app.get("/", (req, res) => {
   res.send("✅ Server is running. Twilio webhook endpoints are ready.");
 });
 // Route for initial call
-app.post("/voice", (req, res) => {
-  const response = new twiml.VoiceResponse();
-
-  const gather = response.gather({
-    numDigits: 1,
-    timeout: 60,
-    action: "/handle-key", // Twilio will POST here
-  });
-
-  gather.say(
-    "Welcome to JB Warranties Canada support. Press 1 to connect to Alex Bulanov. Press 2 to connect to Sam Arbour. To repeat this message, press star",
-    { bargeIn: true, language:"en-US" }
-  );
-
-  response.say("We did not receive any input. Goodbye!", language="en-US");
-  
+  voice 
   handleKey
 
   res.type("text/xml");
   res.send(response.toString());
-});
-
-// Route for handling keypress
 
 
-// Railway needs this
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`✅ Server running on ${PORT}`);

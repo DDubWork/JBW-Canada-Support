222e27a5052a9a4dce31833c6932553e6c091ed7
Damian <dwolecki@jbwarranties.com>
2025-10-03 14:44:23 -0400

    Changes

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const handleVoiceKey = require("./handleVoiceKey.js");
-const voice = require("./voice.js");
 
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -13,7 +12,22 @@ app.get("/", (req, res) => {
   res.send("✅ Server is running. Twilio webhook endpoints are ready.");
 });
 // Route for initial call
-  voice 
+  const voice = app.post("/voice", (req, res) => {
+  const response = new twiml.VoiceResponse();
+
+  const gather = response.gather({
+    numDigits: 1,
+    timeout: 60,
+    action: "/handle-key", // Twilio will POST here
+  });
+
+  gather.say(
+    "Welcome to JB Warranties Canada support. Press 1 to connect to Alex Bulanov. Press 2 to connect to Sam Arbour. To repeat this message, press star",
+    { bargeIn: true, language:"en-US" }
+  );
+
+  response.say("We did not receive any input. Goodbye!", language="en-US");
+}); 
   handleVoiceKey
 
 const PORT = process.env.PORT || 3000;

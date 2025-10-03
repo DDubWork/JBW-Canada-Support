e0bae011ccd16362a2693a5de524dc5db92cc55d
Damian <dwolecki@jbwarranties.com>
2025-10-03 12:12:25 -0400

    Updating handleKey to be a standalone function in its own file and importing it into server.js

---
+++ handle-key.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const { twiml } = require("twilio");
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+const handleKey = app.post("/handle-key", (req, res) => {
+console.log("🚀 Handling key press...");
+  const response = new twiml.VoiceResponse();
+  const digit = req.body.Digits;
+
+  if (digit === "1") {
+    response.say("Please hold for Alex Bulanov", language="en-US");
+    response.dial("+12892007026");
+  } else if (digit === "2") {
+    response.say("Please hold for Sam Arbour", language="en-US");
+    response.dial("+12894235553");
+  } else if (digit === "*") {
+    response.redirect("/voice");
+  }
+
+  res.type("text/xml");
+  res.send(response.toString());
+});
+
+export default handleKey;
--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const handleKey = require("./handle-key.js");
+
 console.log("🚀 Starting server.js...");
 
 
@@ -28,29 +30,15 @@ app.post("/voice", (req, res) => {
   );
 
   response.say("We did not receive any input. Goodbye!", language="en-US");
+  
+  handleKey
 
   res.type("text/xml");
   res.send(response.toString());
 });
 
 // Route for handling keypress
-app.post("/handle-key", (req, res) => {
-  const response = new twiml.VoiceResponse();
-  const digit = req.body.Digits;
 
-  if (digit === "1") {
-    response.say("Please hold for Alex Bulanov", language="en-US");
-    response.dial("+12892007026");
-  } else if (digit === "2") {
-    response.say("Please hold for Sam Arbour", language="en-US");
-    response.dial("+12894235553");
-  } else if (digit === "*") {
-    response.redirect("/voice");
-  }
-
-  res.type("text/xml");
-  res.send(response.toString());
-});
 
 // Railway needs this
 const PORT = process.env.PORT || 3000;
